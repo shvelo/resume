047e74a58d87ed6474e168f71e67d3cf92ca3c39
Nick Shvelidze <captain@pirrate.me>
2014-11-24 01:10:37 +0400

    Mathematical!

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp = require('gulp');
-var markdown = require('gulp-markdown');
-var fileinclude = require('gulp-file-include');
+var gulp = require('gulp'),
+	markdown = require('gulp-markdown'),
+	sass = require('gulp-sass'),
+	fileinclude = require('gulp-file-include');
 
 gulp.task('default', ['html']);
 
@@ -10,12 +11,25 @@ gulp.task('md', function() {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('html', ['md'], function(){
+gulp.task('html', ['md', 'img', 'css'], function(){
 	return gulp.src('index.html')
 	    .pipe(fileinclude('@@'))
 	    .pipe(gulp.dest('dist'));
 });
 
+gulp.task('css', function(){
+	return gulp.src("sass/*")
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('img', function(){
+	return gulp.src('img/*')
+		.pipe(gulp.dest('dist/img'));
+})
+
 gulp.task('watch', ['default'], function(){
 	gulp.watch('resume.md', ['default']);
+	gulp.watch('sass/*', ['css']);
+	gulp.watch('img/*', ['img']);
 });
--- resume.md
+++ resume.md
@@ -1,7 +1,32 @@
-# Nikoloz Shvelidze
-## Mobile / Web developer
+![](img/mugshot.jpg)
 
+# Nick Shvelidze
+### Mobile / Web developer
+
+## Links
+- [GitHub profile](https://github.com/shvelo)
+- [Google+](https://plus.google.com/+NickShvelidze)
+- [Twitter](https://twitter.com/shvelo96)
+- [CoderBits](https://coderbits.com/shvelo)
+
+## Skills:
 - Android development
 - Full-stack web development
-- Source control
-- Other stuff
+- Java, Node.js, Ruby
+- Angular.js, jQuery
+- SASS, CSS
+- Source control - Git
+- Advanced Linux
+
+## Employment
+- Front-End Web Developer  
+Steady Logic - 2014
+- Web / Android Developer  
+LemonDo - 2013-2014
+- Web Developer  
+Omedia - 2013
+
+## Education
+- B.Sc. in Computer and Electrical Engineering  
+Agricultural University of Georgia  
+2014-present
