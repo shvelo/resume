33b122bb375816701e1f77e193fb93e695941c33
Nick Shvelidze <captain@pirrate.me>
2019-12-22 23:48:02 +0400

    Migrate to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 var gulp = require('gulp'),
-	markdown = require('gulp-markdown'),
-	sass = require('gulp-sass'),
-	rename = require('gulp-rename'),
-	wrap = require('gulp-wrap')
-    exec = require('gulp-exec')
+    markdown = require('gulp-markdown'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    wrap = require('gulp-wrap'),
+    exec = require('gulp-exec'),
     frontMatter = require('gulp-front-matter');
 
-gulp.task('default', ['html', 'index', 'pdf']);
+gulp.task('css', function () {
+    return gulp.src("sass/*")
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
 
-gulp.task('html', ['css'], function(){
-	return gulp.src('resume.md')
+gulp.task('html', function () {
+    return gulp.src('resume.md')
         .pipe(frontMatter({
             property: 'matter',
             remove: true
@@ -17,28 +21,25 @@ gulp.task('html', ['css'], function(){
         .pipe(markdown())
         .pipe(wrap({ src: 'template.html' }))
         .pipe(rename({ extname: '.html' }))
-	    .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task('index', ['html'], function(){
+gulp.task('index', function () {
     return gulp.src('resume.html')
         .pipe(rename('index.html'))
         .pipe(gulp.dest('.'));
 });
 
-gulp.task('css', function(){
-	return gulp.src("sass/*")
-        .pipe(sass())
-        .pipe(gulp.dest('css'));
-});
-
-gulp.task('watch', ['default'], function(){
-	gulp.watch('resume*.md', ['default']);
-	gulp.watch('sass/*', ['css']);
-});
-
-gulp.task('pdf', ['html'], function(){
+gulp.task('pdf', function () {
     return gulp.src("resume*.html")
         .pipe(exec("wkhtmltopdf <%= file.path %> <%= file.path.replace('.html','') %>.pdf"))
         .pipe(exec.reporter());
 });
+
+gulp.task('default', gulp.series('css', 'html', 'index', 'pdf'));
+
+gulp.task('watch', gulp.series('default', function () {
+    gulp.watch('resume*.md', gulp.task('default'));
+    gulp.watch('sass/*', gulp.task('default'));
+    gulp.watch('template.html', gulp.task('default'));
+}));
