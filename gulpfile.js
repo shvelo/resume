fdd751ba29b09a1eb3970c0f68080e06cffc0998
Nick Shvelidze <captain@pirrate.me>
2022-02-08 23:47:42 +0400

    Update build files

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-var gulp = require('gulp'),
-    markdown = require('gulp-markdown'),
-    sass = require('gulp-sass'),
-    rename = require('gulp-rename'),
-    wrap = require('gulp-wrap'),
-    exec = require('gulp-exec'),
-    frontMatter = require('gulp-front-matter');
+const gulp = require('gulp');
+const markdown = require('gulp-markdown');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const wrap = require('gulp-wrap');
+const exec = require('gulp-exec');
+const frontMatter = require('gulp-front-matter');
 
-gulp.task('css', function () {
-    return gulp.src("sass/*")
+gulp.task('css', () =>
+    gulp.src("sass/*")
         .pipe(sass())
-        .pipe(gulp.dest('css'));
-});
+        .pipe(gulp.dest('css'))
+);
 
-gulp.task('html', function () {
-    return gulp.src('resume.md')
+gulp.task('html', () =>
+    gulp.src('resume.md')
         .pipe(frontMatter({
             property: 'matter',
             remove: true
@@ -21,24 +21,24 @@ gulp.task('html', function () {
         .pipe(markdown())
         .pipe(wrap({ src: 'template.html' }))
         .pipe(rename({ extname: '.html' }))
-        .pipe(gulp.dest('.'));
-});
+        .pipe(gulp.dest('.'))
+);
 
-gulp.task('index', function () {
-    return gulp.src('resume.html')
+gulp.task('index', () =>
+    gulp.src('resume.html')
         .pipe(rename('index.html'))
-        .pipe(gulp.dest('.'));
-});
+        .pipe(gulp.dest('.'))
+);
 
-gulp.task('pdf', function () {
-    return gulp.src("resume*.html")
-        .pipe(exec("wkhtmltopdf --enable-local-file-access <%= file.path %> <%= file.path.replace('.html','') %>.pdf"))
-        .pipe(exec.reporter());
-});
+gulp.task('pdf', () =>
+    gulp.src("resume*.html")
+        .pipe(exec((file) => `wkhtmltopdf --enable-local-file-access "${file.path}" "${file.path.replace('.html', '.pdf')}"`))
+        .pipe(exec.reporter())
+);
 
 gulp.task('default', gulp.series('css', 'html', 'index', 'pdf'));
 
-gulp.task('watch', gulp.series('default', function () {
+gulp.task('watch', gulp.series('default', () => {
     gulp.watch('resume*.md', gulp.task('default'));
     gulp.watch('sass/*', gulp.task('default'));
     gulp.watch('template.html', gulp.task('default'));
